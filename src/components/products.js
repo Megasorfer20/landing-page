471a9b97fa1ce7ba567a8e7fb6eb9dffbb9b94d7
Esteban Dario Ruiz <edrp.campus15@gmail.com>
2024-03-12 10:12:50 -0500

    products update

--- src/components/products.js
+++ src/components/products.js
@@ -7,30 +7,35 @@ import tonicoMechudosFoto5 from "../images/tonico4.png";
 import tonicoMechudosFoto6 from "../images/tonicoMechudos.jpeg";
 
 function Products() {
+
+  const chamgePhotoHandler = (image) => {
+    
+  }
+
   return (
     <div className="Products">
       <div className="big-product">
         <img src="" alt="" />
       </div>
       <div className="allMedia">
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
+        <button className="img-container" onClick={()=>{ chamgePhotoHandler(tonicoMechudosFoto1)  }}>
+          <img src={tonicoMechudosFoto1} alt="Tónico Mechudos" />
+        </button>
+        <button className="img-container" onClick={()=>{ chamgePhotoHandler(tonicoMechudosFoto2)  }}>
+          <img src={tonicoMechudosFoto2} alt="Tónico Mechudos" />
+        </button>
+        <button className="img-container" onClick={()=>{ chamgePhotoHandler(tonicoMechudosFoto3)  }}>
+          <img src={tonicoMechudosFoto3} alt="Tónico Mechudos" />
+        </button>
+        <button className="img-container" onClick={()=>{ chamgePhotoHandler(tonicoMechudosFoto4)  }}>
+          <img src={tonicoMechudosFoto4} alt="Tónico Mechudos" />
+        </button>
+        <button className="img-container" onClick={()=>{ chamgePhotoHandler(tonicoMechudosFoto5)  }}>
+          <img src={tonicoMechudosFoto5} alt="Tónico Mechudos" />
+        </button>
+        <button className="img-container" onClick={()=>{ chamgePhotoHandler(tonicoMechudosFoto6)  }}>
+          <img src={tonicoMechudosFoto6} alt="Tónico Mechudos" />
+        </button>
       </div>
     </div>
   );
